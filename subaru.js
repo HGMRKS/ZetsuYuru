dafc31322ef900fea9ea6a223b0c3811ecb75469
HGMRKS <97036652+HGMRKS@users.noreply.github.com>
2022-01-06 03:19:51 +0900

    second commit

--- subaru.js
+++ subaru.js
@@ -1,4 +1,4 @@
-//現状、復旧/発生でも時間が15分以内なら反応してしまう。
+﻿//現状、復旧/発生でも時間が15分以内なら反応してしまう。
 //正規表現なりなんなりで判定修正予定。
 //それぞれの時間になったら自動停止するようにしたい。
 
@@ -26,7 +26,7 @@ if(ticket.match(/#/)){
 
 let subaruKill=function(){
 
-const sssKill = window.open("https://togo.tmz.itmac.jp/redmine/projects/ope_network/issues?set_filter=1&status_id=7&description=~L-SSS", "pageSSS");
+const sssKill = window.open("https://togo.tmz.itmac.jp/redmine/projects/ope_network/issues?utf8=%E2%9C%93&set_filter=1&sort=id%3Adesc&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&v%5Bstatus_id%5D%5B%5D=7&f%5B%5D=description&op%5Bdescription%5D=%21%7E&v%5Bdescription%5D%5B%5D=%E9%96%A2%E4%BF%82%E4%BC%9A%E7%A4%BE&f%5B%5D=cf_6&op%5Bcf_6%5D=%7E&v%5Bcf_6%5D%5B%5D=SSS&f%5B%5D=&c%5B%5D=cf_2&c%5B%5D=cf_3&c%5B%5D=cf_4&c%5B%5D=cf_5&c%5B%5D=cf_1&c%5B%5D=subject&c%5B%5D=status&c%5B%5D=priority&c%5B%5D=project&group_by=&t%5B%5D=", "pageSSS");
 sssKill.addEventListener('DOMContentLoaded', function(){
 
 	let almExistence=sssKill.document.getElementsByClassName("nodata")[0]!=null;
